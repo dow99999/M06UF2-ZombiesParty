281a4dd42b98c1590900eaee2db5d54f47ee0159
Diego Muñoz Jorquera <38563257+dow99999@users.noreply.github.com>
2020-11-30 21:20:26 +0100

    Inicializacion de la matriz tablero

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
+const GESP_T = 'g';
+const ZOMBIE_T = 'z';
+const ESTRELLA_T = 'e';
+const DOBLAR_T = 'd';
+const MITAD_T = 'm';
+const VIDA_T = 'v';
+
+const GESP_D = 'G';
+const ZOMBIE_D = 'Z';
+const ESTRELLA_D = 'E';
+const DOBLAR_D = 'D';
+const MITAD_D = 'M';
+const VIDA_D = 'V';
+
+/**
+ * Procedimiento que inicializa la matriz del tablero con espacios 'vacios'
+ * @param {int} taulerh alto del tablero
+ * @param {int} taulerw ancho del tablero
+ * @param {Object} tauler tablero 
+ */
+function initTauler(taulerh, taulerw, tauler){
+  for(let y = 0; y < taulerh; y++){
+    tauler.matriu[y] = new Array();
+    for(let x = 0; x < taulerw; x++){
+      tauler.matriu[y][x] = GESP_T;
+    }
+  }
+}
+
 function joc(){
   let tauler = {
-    matriu: []
+    h: 5,
+    w: 5,
+    matriu: [],
+    print: function(){
+      let aux = "";
+      
+      for(let y = 0; y < this.h; y++){
+        for(let x = 0; x < this.w; x++){
+          aux += this.matriu[y][x];
+        }
+        aux += "\n";
+      }
+
+      return aux;
+    }
   };
+
+  initTauler(tauler.h, tauler.w, tauler);
 }
 
 function main(){
